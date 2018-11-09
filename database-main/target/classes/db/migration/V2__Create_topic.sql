36fbe1ef6fbe790f51352f6daf157a14381690d1
Markus Dreyer <markus.a.dreyer@gmail.com>
2018-11-09 12:44:48 +0100

    deleted .gitignore files

--- database-main/target/classes/db/migration/V2__Create_topic.sql
+++
@@ -1,9 +0,0 @@
-create table if not exists topic (
-  id          SERIAL PRIMARY key,
-  title VARCHAR not NULL UNIQUE
-);
-
-INSERT INTO topic VALUES
-                         (DEFAULT , 'Science'),
-                         (DEFAULT , 'Programming'),
-                         (DEFAULT , 'Hacking');
--- database-main/target/classes/db/migration/V6__Create_conference_talk.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists CONFERENCE_TALK (
-  id          SERIAL PRIMARY key,
-  title       varchar,
-  description text,
-  topic varchar,
-  foreign key (topic) REFERENCES topic(title)
-);
